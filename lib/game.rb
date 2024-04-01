5be28963315f9b575ec395fbaefb8c3361889208
Trevor Pierce <yeahtrevorpierce@gmail.com>
2024-04-01 16:42:20 -0500

    Select word from dictionary file upon game initialization

--- lib/game.rb
+++ lib/game.rb
@@ -3,4 +3,17 @@
 # The game state for Hangman
 class Game
   attr_reader :word, :guesses, :correct_letters
+
+  def initialize
+    @word = select_word
+  end
+
+  private
+
+  def select_word
+    File.readlines('google-10000-english-no-swears.txt')
+        .select { |line| line.chomp.length.between?(5, 12) }
+        .sample
+        .chomp
+  end
 end
