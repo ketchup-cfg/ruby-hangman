ab6193c48363715fbae8007d73b30012cd4c8835
Trevor Pierce <yeahtrevorpierce@gmail.com>
2024-04-02 13:49:44 -0500

    Add additional game methods for keeping track of progress

--- lib/game.rb
+++ lib/game.rb
@@ -2,12 +2,30 @@
 
 # The game state for Hangman
 class Game
+  MAX_GUESSES = 11
+
   attr_reader :word, :guesses, :correct_letters
 
   def initialize
     @word = select_word
     @guesses = []
     @correct_letters = '_' * @word.length
+  end
+
+  def guesses_left
+    MAX_GUESSES - @guesses.length
+  end
+
+  def won?
+    !@correct_letters.include?('_')
+  end
+
+  def guess(guess)
+    (0...word.length).each do |i|
+      correct_letters[i] = guess[i] if guess[i] == word[i]
+    end
+
+    guesses << guess
   end
 
   private
